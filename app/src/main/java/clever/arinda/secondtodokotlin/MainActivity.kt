b39057b938718ab10aa96ffe77bfd0fbe8cee300
Arinda Clever <48243275+cleverclope@users.noreply.github.com>
2021-11-14 13:24:06 +0300

    kotlin study again

---
+++ app/src/main/java/clever/arinda/secondtodokotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package clever.arinda.secondtodokotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
